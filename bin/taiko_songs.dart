4544fbfef0e9db6ad062e490b5b15491063893eb
AoEiuV020 <aoeiuv020@gmail.com>
2023-08-17 16:21:07 +0800

    下载解析ns2 pass的歌名

--- bin/taiko_songs.dart
+++ bin/taiko_songs.dart
@@ -7,12 +7,13 @@ import 'package:html/parser.dart';
 import 'package:path/path.dart' as path;
 
 Future<void> main(List<String> arguments) async {
-  var url = 'https://wikiwiki.jp/taiko-fumen/%E4%BD%9C%E5%93%81/NS2';
+  var url =
+      'https://wikiwiki.jp/taiko-fumen/%E4%BD%9C%E5%93%81/NS2/%E5%A4%AA%E9%BC%93%E3%83%9F%E3%83%A5%E3%83%BC%E3%82%B8%E3%83%83%E3%82%AF%E3%83%91%E3%82%B9';
   var folder = Directory.systemTemp;
   folder = Directory(path.join(folder.path, 'taiko_songs'));
   await folder.create();
-  var htmlFile = File(path.join(folder.path, 'ns2.html'));
-  var songFile = File(path.join(folder.path, 'ns2.txt'));
+  var htmlFile = File(path.join(folder.path, 'ns2pass.html'));
+  var songFile = File(path.join(folder.path, 'ns2pass.txt'));
   String body;
   if (!await htmlFile.exists()) {
     print('download html');
@@ -25,17 +26,33 @@ Future<void> main(List<String> arguments) async {
     body = await htmlFile.readAsString();
   }
   var root = parse(body);
-  var songList = root.querySelectorAll(
-      "#content > div > table > tbody > tr > td:nth-child(1)");
+  var tableList = root.querySelectorAll('table');
   var write = songFile.openWrite();
-  for (var element in songList) {
-    var nameElement = element.querySelector('strong');
-    if (nameElement == null) {
+  for (var table in tableList) {
+    var thead = table.querySelector('thead');
+    if (thead == null) {
       continue;
     }
-    var name = nameElement.text;
-    print(name);
-    write.writeln(name);
+    bool skip = true;
+    for (var td in thead.querySelectorAll('tr > td')) {
+      if (td.text.trim() == '曲名') {
+        skip = false;
+      }
+    }
+    if (skip) {
+      continue;
+    }
+    var songList = table.querySelectorAll(
+        'tbody > tr > td:nth-child(2)');
+    for (var element in songList) {
+      var nameElement = element.querySelector('strong');
+      if (nameElement == null) {
+        continue;
+      }
+      var name = nameElement.text;
+      print(name);
+      write.writeln(name);
+    }
   }
   await write.close();
 }
