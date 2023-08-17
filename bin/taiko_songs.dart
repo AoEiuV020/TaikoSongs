6724e8f3666269714667845aaa07850f539a5b9b
AoEiuV020 <aoeiuv020@gmail.com>
2023-08-17 16:05:03 +0800

    下载解析ns2的歌名

--- bin/taiko_songs.dart
+++ bin/taiko_songs.dart
@@ -7,12 +7,12 @@ import 'package:html/parser.dart';
 import 'package:path/path.dart' as path;
 
 Future<void> main(List<String> arguments) async {
-  var url = 'https://wikiwiki.jp/taiko-fumen/%E4%BD%9C%E5%93%81/NS1';
+  var url = 'https://wikiwiki.jp/taiko-fumen/%E4%BD%9C%E5%93%81/NS2';
   var folder = Directory.systemTemp;
   folder = Directory(path.join(folder.path, 'taiko_songs'));
   await folder.create();
-  var htmlFile = File(path.join(folder.path, 'ns1.html'));
-  var songFile = File(path.join(folder.path, 'ns1.txt'));
+  var htmlFile = File(path.join(folder.path, 'ns2.html'));
+  var songFile = File(path.join(folder.path, 'ns2.txt'));
   String body;
   if (!await htmlFile.exists()) {
     print('download html');
@@ -26,14 +26,16 @@ Future<void> main(List<String> arguments) async {
   }
   var root = parse(body);
   var songList = root.querySelectorAll(
-      "#content > div.h-scrollable > table > tbody > tr > td:nth-child(1)");
+      "#content > div > table > tbody > tr > td:nth-child(1)");
   var write = songFile.openWrite();
   for (var element in songList) {
     var nameElement = element.querySelector('strong');
     if (nameElement == null) {
       continue;
     }
-    write.writeln(nameElement.text);
+    var name = nameElement.text;
+    print(name);
+    write.writeln(name);
   }
   await write.close();
 }
