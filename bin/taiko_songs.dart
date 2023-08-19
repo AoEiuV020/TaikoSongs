5e71d20165b5e4d9c3a2257441b08a85ba2ab321
AoEiuV020 <aoeiuv020@gmail.com>
2023-08-19 14:44:58 +0800

    打印各分类对应的数量

--- bin/taiko_songs.dart
+++ bin/taiko_songs.dart
@@ -7,8 +7,7 @@ import 'package:path/path.dart' as path;
 import 'package:taiko_songs/src/parser/song_parser.dart';
 
 Future<void> main(List<String> arguments) async {
-  var url =
-      'https://wikiwiki.jp/taiko-fumen/%E4%BD%9C%E5%93%81/NS2';
+  var url = 'https://wikiwiki.jp/taiko-fumen/%E4%BD%9C%E5%93%81/NS2';
   var folder = Directory.systemTemp;
   folder = Directory(path.join(folder.path, 'taiko_songs'));
   await folder.create();
@@ -29,9 +28,12 @@ Future<void> main(List<String> arguments) async {
   var songList = parser.parseSongList(body);
   var write = songFile.openWrite();
   int index = 0;
+  Map<String, int> categoryMap = {};
   await songList.forEach((it) {
     print("${++index} $it");
+    categoryMap[it.category] = (categoryMap[it.category] ?? 0) + 1;
     write.writeln(it.name);
   });
+  print(categoryMap);
   await write.close();
 }
