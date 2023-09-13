04838103a842d1d173e66e1812e2622c50a71db7
AoEiuV020 <aoeiuv020@gmail.com>
2023-09-13 11:32:40 +0800

    优先展示现行作品

--- lib/src/parser/collection_parser.dart
+++ lib/src/parser/collection_parser.dart
@@ -5,14 +5,19 @@ import 'package:taiko_songs/src/parser/base.dart';
 class CollectionParser extends Parser {
   Stream<ReleaseItem> parseList(String baseUrl, String doc) {
     var root = parse(doc);
-    var aList = root.querySelectorAll('#content ul li a');
+    // 現行作品
+    var aList1 = root.querySelectorAll('#content table > tbody > tr > td > a');
+    var aList2 = root.querySelectorAll('#content ul li a');
     var urlPrefix = 'https://wikiwiki.jp/taiko-fumen/%E4%BD%9C%E5%93%81/';
-    return Stream.fromIterable(aList)
+
+    final Set<String> existsSet = {};
+    return Stream.fromIterable(aList1 + aList2)
         .map((event) => ReleaseItem(event.text, getAbsHref(baseUrl, event)))
         .where((event) => event.url.startsWith(urlPrefix))
         .map((event) => ['ENG', '中文', '北米版', '亜州版', '韓国版'].contains(event.name)
             ? ReleaseItem.fromUrl(event.url)
             : event)
+        .where((event) => existsSet.add(event.url))
         .distinct((a, b) => a.url == b.url);
   }
 }
