547ad0fa0100071170f5617650ce333a2d54eda1
AoEiuV020 <aoeiuv020@gmail.com>
2023-09-30 18:09:04 +0800

    未翻译文本添加复制全部按钮

--- lib/src/view/miss_translated_text_view.dart
+++ lib/src/view/miss_translated_text_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:taiko_songs/src/view/translated_text_view.dart';
 
 class MissTranslatedTextView extends StatelessWidget {
@@ -6,23 +8,39 @@ class MissTranslatedTextView extends StatelessWidget {
 
   static const routeName = '/miss_translated_text';
 
-  String makeText() {
-    return TranslatedText.getMissTextSet().join('\n');
-  }
-
   @override
   Widget build(BuildContext context) {
+    final text = TranslatedText.getMissTextSet().join('\n');
     return Scaffold(
       appBar: AppBar(
         title: const Text('未翻译文本'),
       ),
-      body: SizedBox(
-        width: double.infinity,
-        height: double.infinity,
-        child: Center(
-          child: SelectableText(
-            makeText(),
-          ),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: ElevatedButton(
+                onPressed: () async {
+                  await Clipboard.setData(ClipboardData(text: text));
+                  EasyLoading.showToast(
+                    "复制完成",
+                    duration: const Duration(milliseconds: 500),
+                  );
+                },
+                child: const Text('复制全部'),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: SelectableText(
+                  text,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
